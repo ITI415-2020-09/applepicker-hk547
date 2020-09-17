2a01dad4ac4871f758597f5d49e97bf3eb54ee01
hk547 <60270709+hk547@users.noreply.github.com>
2020-09-17 12:50:29 -0400

    basket script2 moving
    search for
    input.mousePosititon
    mousePos2D
    ScreenToWorldPoint()
    mousePos3D
    CameraScreenToWorldPoint()

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -13,6 +13,15 @@ public class Basket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // get current screen position of the mouse from Input
+        Vector3 mousePos2D = Input.mousePosition;
+        // camera's position sets how far to push the mose into 3D
+        mousePos2D.x = -Camera.main.transform.position.x;
+        // convert the point from 2D screen space into 3D game world space
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        // mve the x poosition of this basket to the x position of the mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
     }
 }
