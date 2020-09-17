a52e23ceb7f4649f4995008672e2dca817fc87f2
hk547 <60270709+hk547@users.noreply.github.com>
2020-09-17 13:10:36 -0400

    Basket Script3 Collision
    When tag collides with something do somthing.
    OnCollisionEnter method / collidedWith

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -15,13 +15,23 @@ public class Basket : MonoBehaviour
     {
         // get current screen position of the mouse from Input
         Vector3 mousePos2D = Input.mousePosition;
-        // camera's position sets how far to push the mose into 3D
-        mousePos2D.x = -Camera.main.transform.position.x;
+        // camera's position sets how far to push the mouse into 3D
+        mousePos2D.z = -Camera.main.transform.position.z;
         // convert the point from 2D screen space into 3D game world space
         Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
         // mve the x poosition of this basket to the x position of the mouse
         Vector3 pos = this.transform.position;
         pos.x = mousePos3D.x;
         this.transform.position = pos;
+    }
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        //Find out what hit this basket
+        GameObject collidedWith = coll.gameObject;
+        if(collidedWith.tag == "Apple")
+        {
+            Destroy(collidedWith);
+        }
     }
 }
